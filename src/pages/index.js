b15b62c8755cfece9f4794756dfcbb6e90573afe
mhe <mail@marcus-herrmann.com>
2019-05-12 16:08:52 +0200

    Again triggering netlify build -.-

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import Layout from "../components/layout";
-import Intro from "../components/intro";
+import Intro from "../components/Intro";
 import Challenges from "../components/challenges";
 import About from "../components/about";
 import Patterns from "../components/patterns";
